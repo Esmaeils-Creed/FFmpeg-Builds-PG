773dcd32c9711b6f0f0d6d2c0b4885f6f632c93f
Mohammad Amini <92734921+Esmaeils-Creed@users.noreply.github.com>
2026-09-24 01:16:39 +0330

    Update LLVM and CUDA installation commands

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -42,8 +42,7 @@ ffbuild_dockerbuild() {
     make -C "$nvdir" PREFIX="$FFBUILD_PREFIX" install
 
 	pip3 install --break-system-packages bin2c
-	curl -fsSL https://apt.llvm.org/llvm.sh | bash -s -- 22 && update-alternatives --install /usr/bin/clang clang "$(command -v clang-22)" 100 && update-alternatives --install /usr/bin/clang++ clang++ "$(command -v clang++-22)" 100
-    curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && rm cuda-keyring_1.1-1_all.deb && apt-get update && apt-get install -y --no-install-recommends cuda-toolkit-13-0
+	curl -fsSL https://apt.llvm.org/llvm.sh | bash -s -- 22 && ln -sf "$(command -v clang-22)" /usr/bin/clang && ln -sf "$(command -v clang++-22)" /usr/bin/clang++ && clang --version | head -2    curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && rm cuda-keyring_1.1-1_all.deb && apt-get update && apt-get install -y --no-install-recommends cuda-toolkit-13-0
 	export PATH="/usr/local/cuda-13.0/bin:$PATH"
 
     mkdir build && cd build
