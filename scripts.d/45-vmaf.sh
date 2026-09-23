758dfc847d468b3c731de9d9afbb473b2e7b1029
Mohammad Amini <92734921+Esmaeils-Creed@users.noreply.github.com>
2026-09-23 23:50:45 +0330

    Modify 45-vmaf.sh for LLVM and CUDA installation
    Updated installation steps for LLVM and CUDA in the script.

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -41,9 +41,11 @@ ffbuild_dockerbuild() {
     fi
     make -C "$nvdir" PREFIX="$FFBUILD_PREFIX" install
 
-	pip3 install --break-system-packages bin2c
-
-    mkdir build && cd build
+	curl -fsSL https://apt.llvm.org/llvm.sh | bash -s -- 22 && update-alternatives --install /usr/bin/clang clang "$(command -v clang-22)" 100 && update-alternatives --install /usr/bin/clang++ clang++ "$(command -v clang++-22)" 100
+    curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && rm cuda-keyring_1.1-1_all.deb && apt-get update && apt-get install -y --no-install-recommends cuda-nvcc-13-0 cuda-cudart-dev-13-0
+	export PATH="/usr/local/cuda-13.0/bin:$PATH"
+	
+	mkdir build && cd build
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
