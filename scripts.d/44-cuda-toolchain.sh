db1997ba8140c696bb4e5c13a1b994804b4c263d
Mohammad Amini <92734921+Esmaeils-Creed@users.noreply.github.com>
2026-09-24 00:21:05 +0330

    Update CUDA installation to toolkit version 13.0

--- scripts.d/44-cuda-toolchain.sh
+++ scripts.d/44-cuda-toolchain.sh
@@ -11,5 +11,5 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     set -e
     curl -fsSL https://apt.llvm.org/llvm.sh | bash -s -- 22 && update-alternatives --install /usr/bin/clang clang "$(command -v clang-22)" 100 && update-alternatives --install /usr/bin/clang++ clang++ "$(command -v clang++-22)" 100
-    curl -fsSL -o /tmp/cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i /tmp/cuda-keyring.deb && rm /tmp/cuda-keyring.deb && apt-get update && apt-get install -y --no-install-recommends cuda-nvcc-12-6 cuda-cudart-dev-12-6
+    curl -fsSL -o /tmp/cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i /tmp/cuda-keyring.deb && rm /tmp/cuda-keyring.deb && apt-get update && apt-get install -y --no-install-recommends cuda-toolkit-13.0
 }
