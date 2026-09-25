1c3f1b3107ce40ac1f52ec273414f7504061e44d
Mohammad Amini <92734921+Esmaeils-Creed@users.noreply.github.com>
2026-09-25 13:08:03 +0330

    Comment out clang command in 45-vmaf.sh
    Comment out the clang command for device code compilation.

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -77,7 +77,7 @@ ffbuild_dockerbuild() {
     ls -l /usr/bin/clang /usr/bin/clang++
 	grep -rlZ '#include "feature_collector.h"' libvmaf/src/feature/cuda/ | xargs -0 perl -0777 -pi -e 's/#include "feature_collector\.h"/#ifndef DEVICE_CODE\n#include "feature_collector.h"\n#endif/g'
 	sed -i "s|'-I', '../src',|'-I', '../libvmaf/src',|; s|'-I', '../include',|'-I', '../libvmaf/include',|; s|'-I', '../src/feature',|'-I', '../libvmaf/src/feature',|; s|'-I', '../src/' + cuda_dir,|'-I', '../libvmaf/src/' + cuda_dir, '-I', '$FFBUILD_PREFIX/include',|" libvmaf/src/meson.build
-	clang --cuda-gpu-arch=sm_75 --cuda-device-only -E -H ../libvmaf/src/feature/cuda/integer_adm/adm_dwt2.cu -I ./src -I ../libvmaf/src -I ../libvmaf/include -I ../libvmaf/src/feature -I ../libvmaf/src/cuda/ -I "$FFBUILD_PREFIX/include" -DDEVICE_CODE 2>&1 | grep -E '^\.+ |fatal error'; true
+	#clang --cuda-gpu-arch=sm_75 --cuda-device-only -E -H ../libvmaf/src/feature/cuda/integer_adm/adm_dwt2.cu -I ./src -I ../libvmaf/src -I ../libvmaf/include -I ../libvmaf/src/feature -I ../libvmaf/src/cuda/ -I "$FFBUILD_PREFIX/include" -DDEVICE_CODE 2>&1 | grep -E '^\.+ |fatal error'; true
 	mkdir build && cd build
 
     local myconf=(
